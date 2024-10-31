e88b5c4e24a4231e0f538e692ffa624320429c75
lilybrongo <144718640+lilybrongo@users.noreply.github.com>
2024-10-31 15:39:11 -0400

    first attempt code.js

--- code.js
+++ code.js
@@ -1,3 +1,17 @@
-function convertToAdjList(adjMatrix) {
-    return adjMatrix;
+function convertToAdjList(adjMatrix) 
+{
+  let adjList = [];
+  for (let row = 0; row < adjMatrix.length; row++) 
+  {
+    let edgeLink = [];
+    for (let column = 0; column < adjMatrix.length; column++) 
+    {
+      if (adjMatrix[row][column] == 1) 
+      {
+        edgeLink.push(column);
+      }
+    }
+    adjList.push(edgeLink);
+  }
+  return adjList;
 }
